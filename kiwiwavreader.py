c51520ce8a02156d2c25c794f93bb2d621b62015
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-06-03 12:10:59 +0200

    kiwiwavreader: improve error messages

--- kiwiwavreader.py
+++ kiwiwavreader.py
@@ -31,19 +31,19 @@ def _read_chunk(data):
 def read_kiwiwav(f):
     data = memoryview(f.read())
     if data[0:4] != b"RIFF":
-        raise Exception("not a RIFF")
+        raise Exception(f"{f}: not a RIFF")
     fsize, = struct.unpack("<I", data[4:8])
     if data[8:12] != b"WAVE":
-        raise Exception("not a WAVE")
+        raise Exception(f"{f}: not a WAVE")
     if data[12:16] != b"fmt ":
-        raise Exception("no fmt chunk found")
+        raise Exception(f"{f}: no fmt chunk found")
     lfmt, fmtt, nchannels, sr, br, ba, bits = struct.unpack("<IHHIIHH", data[16:36])
     if lfmt != 16:
-        raise Exception("invalid fmt chunk length")
+        raise Exception(f"{f}: invalid fmt chunk length")
     if nchannels != 2:
-        raise Exception("expected 2 channels")
+        raise Exception(f"{f}: expected 2 channels")
     if bits != 16:
-        raise Exception("expected 16-bit samples")
+        raise Exception(f"{f}: expected 16-bit samples")
 
     chunks_raw = []
     ridx = 36
@@ -51,11 +51,11 @@ def read_kiwiwav(f):
         cl, cdata1 = _read_chunk(data[ridx:])
         ridx += cl
         if cdata1["chunk_type"] != "kiwi":
-            raise Exception("expected kiwi chunk")
+            raise Exception(f"{f}: expected kiwi chunk")
         cl, cdata2 = _read_chunk(data[ridx:])
         ridx += cl
         if cdata2["chunk_type"] != "data":
-            raise Exception("expected data chunk")
+            raise Exception(f"{f}: expected data chunk")
         chunks_raw.append({
             "fix_age": cdata1["fix_age"],
             "gpsns": (cdata1["gpssec"] * int(1e9)) + cdata1["gpsnsec"],
@@ -64,25 +64,25 @@ def read_kiwiwav(f):
 
     chunk_len = len(chunks_raw[0]["data"])
     if len(set(len(x["data"]) for x in chunks_raw)) != 1:
-        raise Exception("unsupported: chunks have different lengths")
+        raise Exception(f"{f}: unsupported: chunks have different lengths")
 
     dt_ns_chunks = [] 
     gpsns_prev = 0
     for i, chunk in enumerate(chunks_raw):
         if i == 0 and chunk["gpsns"] != 0:
-            raise Exception("first chunk has nonzero timestamp")
+            raise Exception(f"{f}: first chunk has nonzero timestamp")
         if chunk["gpsns"] == 0 and i != 0:
-            raise Exception("zero timestamp after start")
+            raise Exception(f"{f}: zero timestamp after start")
         if gpsns_prev > 0:
             if chunk["gpsns"] < gpsns_prev:
-                raise Exception("clock went backwards or didn't run")
+                raise Exception(f"{f}: clock went backwards or didn't run current: {chunk['gpsns']} prev: {gpsns_prev}")
             dt_ns = chunk["gpsns"] - gpsns_prev
             dt_ns_chunks.append(dt_ns)
         gpsns_prev = chunk["gpsns"]
     if len(dt_ns_chunks) == 0:
-        raise Exception("no usable timestamps")
+        raise Exception(f"{f}: no usable timestamps")
     if len(dt_ns_chunks) + 2 != len(chunks_raw):
-        raise Exception("dt_ns_chunks length doesn't fit together with chunks_raw length")
+        raise Exception(f"{f}: dt_ns_chunks length doesn't fit together with chunks_raw length")
     dt_ns_median = np.median(dt_ns_chunks)
     sr_measured = chunk_len / (dt_ns_median * 1e-9)
     sr_ppm = ((sr_measured - sr) / sr) * 1e6
@@ -105,7 +105,7 @@ def read_kiwiwav(f):
             break
         dt = chunks_raw[i+1]["gpsns"] - (chunk["gpsns"] + chunk["duration_ns"])
         if dt != 0:
-            raise Exception(f"chunks {i} and {i+1} are spaced weirdly")
+            raise Exception(f"{f}: chunks {i} and {i+1} are spaced weirdly")
 
     # compute timestamps for each sample
     for chunk in chunks_raw:
@@ -117,7 +117,7 @@ def read_kiwiwav(f):
 
     # last sanity check
     if len(samples_all) != len(timestamps_all):
-        raise Exception("len(samples_all) != len(timestamps_all)")
+        raise Exception(f"{f}: len(samples_all) != len(timestamps_all)")
 
     return TDoARecording(
         samples_all,
